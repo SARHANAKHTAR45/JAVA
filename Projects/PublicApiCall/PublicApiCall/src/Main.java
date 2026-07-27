444b1ec675b57a035a55795f59c8ed8770c46798
Sarhan Akhtar <sarhanakhtar55@gmail.com>
2026-07-27 14:54:36 +0530

    Delete Projects/PublicApiCall/PublicApiCall directory

--- Projects/PublicApiCall/PublicApiCall/src/Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-    public static void main(String[] args) {
-        ApiService api = new ApiService();
-        String message = api.getMessage();
-        System.out.println("Message from API:");
-        System.out.println(message);
-    }
-}
