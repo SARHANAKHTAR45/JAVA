ef1437a6fa58b14801981a75ea0a67f758b615d1
Sarhan Akhtar <sarhanakhtar55@gmail.com>
2026-09-05 16:03:50 +0530

    Add files via upload

---
+++ Projects/Real-Time Chat Application/src/main/java/com/chat/app/controller/ChatController.java
@@ -0,0 +1,25 @@
+package com.chat.app.controller;
+
+/*this controller will recieve all the information from frontend and
+forward it to the clients*/
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.chat.app.model.ChatMessage;
+
+@Controller
+public class ChatController {
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/messages")
+    public ChatMessage sendMessage(ChatMessage message){
+        return message;
+    }
+
+    @GetMapping("/chat")
+    public String chat(){
+        return "chat";
+    }
+}
